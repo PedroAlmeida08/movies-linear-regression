f2af8cba3e4881e73b5bb89b33b678a372e8e48f
PedroAlmeida08 <jp_almeida@id.uff.br>
2024-06-27 18:51:23 -0300

    feat: analysis em R

--- analysis.R
+++ analysis.R
@@ -1,9 +1,21 @@
 df.data <- read.csv('data/dados.csv')
 
-df.regression <- lm(profit ~ duration + Action + Adventure + Animation + 
+df.regression <- lm(profit_norm ~ duration_norm + Action + Adventure + Animation + 
                       Biography + Comedy + Crime + Documentary + Drama + 
                       Family + Fantasy + History + Horror + Music + Musical + 
-                      Mystery + Romance + Sci.Fi + Short + Sport + Thriller + 
+                      Mystery + Romance + Sci.Fi + Sport + Thriller + 
                       War + Western, data=df.data)
 
 summary(df.regression)
+
+# Resíduos do modelo
+res <- residuals(df.regression)
+
+# Quadrados dos erros (resíduos)
+squared_errors <- res^2
+
+# Erro Quadrático Médio (EQM)
+MSE <- mean(squared_errors)
+
+# Exibir o valor do MSE
+print(MSE)
